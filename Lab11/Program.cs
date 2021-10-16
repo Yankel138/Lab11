b6ce7f79773606c23f782d622f23212e57b80837
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-16 12:31:01 +0300

    .

--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -10,27 +10,31 @@ namespace Lab11
     {
         struct Equation
         {
-            public float k;
-            public float b;
+            public double k;
+            public double b;
 
-            public Equation(float k, float b)
+            public Equation(double k, double b)
             {
                 this.k = k;
                 this.b = b;
             }
             public void Root()
             {
-                float x = -b / k;
-                Console.WriteLine($"0 = {k}x + {b}\nx = {x}");
+                double x = -b / k;
+                Console.WriteLine($"0 = {k}x + {b}\nx = {x:f2}");
             }
         }
 
         static void Main(string[] args)
         {
-            Equation eq1 = new Equation(2, 3);
+            Equation eq1 = new Equation(-2, 3);
             eq1.Root();
-            Equation eq2 = new Equation(10, 3);
+            
+            Equation eq2;
+            eq2.k = 12.5;
+            eq2.b = 2.5;
             eq2.Root();
+            
             Console.ReadKey();
         }
     }
