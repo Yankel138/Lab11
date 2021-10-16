f1b773ddc54b02a43cb254ea8180e8f40bac501e
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-16 12:19:11 +0300

    Добавьте файлы проекта.

---
+++ Lab11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        struct Equation
+        {
+            public float k;
+            public float b;
+
+            public Equation(float k, float b)
+            {
+                this.k = k;
+                this.b = b;
+            }
+            public void Root()
+            {
+                float x = -b / k;
+                Console.WriteLine($"0 = {k}x + {b}\nx = {x}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Equation eq1 = new Equation(2, 3);
+            eq1.Root();
+            Equation eq2 = new Equation(10, 3);
+            eq2.Root();
+            Console.ReadKey();
+        }
+    }
+}
